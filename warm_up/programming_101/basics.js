80c00b6c3a1b4e9ba3efa24cab2d19c60b46833e
Rahul <rahulmishra4317@gmail.com>
2025-06-02 01:28:34 +0530

    Learnt Basics of Programming : Programming 101

--- warm_up/programming_101/basics.js
+++ warm_up/programming_101/basics.js
@@ -1,4 +1,25 @@
 const arr = [1,2,3,4,5,]
 console.log("arr" , arr)
+console.log("-----\n")
 
-console.log(arr[5])
+
+//objects in javaScript 
+let obj = {
+    firstName : "Anshul" , 
+    lastName : "Kumar" , 
+    age : 24 , 
+    profession : "Software Engineer" 
+}
+
+
+let fullName = obj["firstName"] + " " + obj.lastName; 
+console.log(fullName);
+console.log("-----\n");
+
+
+
+console.log("New name")
+obj.firstName = "Muskan"
+obj["lastName"] = "Acharya"
+
+console.log("obj" , obj)
