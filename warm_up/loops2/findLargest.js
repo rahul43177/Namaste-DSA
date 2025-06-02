7105e7906c8884ec793ddacc21aba09d6dfac670
Rahul <rahulmishra4317@gmail.com>
2025-06-02 13:24:00 +0530

    Two methods of solving largest element

--- warm_up/loops2/findLargest.js
+++ warm_up/loops2/findLargest.js
@@ -1,3 +1,4 @@
+//first method : Assigning Largest as the smallest first 
 function findLargest(array) {
     let largest = -Infinity;  //largest number : assign as the smallest to get the first number; 
     for(let i = 0;i<array.length;i++) {
@@ -11,3 +12,19 @@ function findLargest(array) {
 let array = [10,18 , 1, -1 , 12 , 34]
 let large = findLargest(array) ;
 console.log(`The largest number in the array is : ${large}`);
+
+//second method : Assign the largest as the first element of the array 
+function findLargest2(array){ 
+    let largest = array[0];
+    for(let i = 1;i<array.length;i+=1) { //First number is assigned to the largest , starting the loop from second
+        if(array[i] > largest) {
+            largest = array[i];
+        }
+    }
+    return largest;
+} 
+
+console.log("\n")
+console.log("----------\n")
+let largeNumber = findLargest2(array)
+console.log(`The largest number in the array is : ${largeNumber}`);
