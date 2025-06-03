52a43cde5a64a2e00613d8470080b876153f4fc4
Rahul <rahulmishra4317@gmail.com>
2025-06-03 17:20:26 +0530

    Find second largest number of the array

--- warm_up/Basics/findSecondLargest.js
+++ warm_up/Basics/findSecondLargest.js
@@ -1,18 +1,20 @@
-function findSecondLargest(arr) {
-    let largestNumber = -Infinity
-    let secondLargestNumber = -Infinity
+const findSecondLargestNumber = arr => {
+    let largestNumber = -Infinity; 
+    let secondLargest = -Infinity; 
 
     for(let i = 0;i<arr.length;i+=1) {
         if(arr[i] > largestNumber) {
-            secondLargestNumber = largestNumber; 
-            largestNumber = arr[i];
-        } else if(arr[i] > secondLargestNumber && arr[i] !=largestNumber) { 
-            secondLargestNumber = arr[i]
+            secondLargest = largestNumber ; 
+            largestNumber = arr[i] ; 
+        } else if(arr[i]!=largestNumber && secondLargest < arr[i]) {
+            secondLargest = arr[i] ; 
         }
     }
-    return secondLargestNumber; 
+    return secondLargest; 
 }
 
+let arr1 = [5,11,23 , 1, ,3 ,9 ] ;
+let arr2= [10,20,30,33]
 
-let arr = [10,20,40,50]
-console.log(findSecondLargest(arr))
+console.log(findSecondLargestNumber(arr1))
+console.log(findSecondLargestNumber(arr2))
