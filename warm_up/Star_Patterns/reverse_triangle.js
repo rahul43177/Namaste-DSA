20c56d04c66fc70d1b6b1b4b32b089b341ce0dc1
Rahul <rahulmishra4317@gmail.com>
2025-06-07 00:20:31 +0530

    Star Patterns Questions

---
+++ warm_up/Star_Patterns/reverse_triangle.js
@@ -0,0 +1,19 @@
+/*
+
+* * * * *
+* * * *
+* * * 
+* * 
+* 
+
+
+*/
+
+let n = 5;
+for(let i =0;i<n;i++) {
+    let rows = "";
+    for(let j =0;j<n-i;j+=1) {
+        rows += "* ";
+    } 
+    console.log(rows)
+}
