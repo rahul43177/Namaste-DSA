1a39e3d0a333617e31a27cfcd9275b7fe85d52e3
Rahul <rahulmishra4317@gmail.com>
2025-06-11 19:04:11 +0530

    Remove duplicates : Leetcode

--- Array-Level-0/removeDuplicates.js
+++ Array-Level-0/removeDuplicates.js
@@ -1,4 +1,5 @@
 /*
+
 Given a sorted array nums, remove duplicates in-place such that each element appears only once and returns the new length.
 Do not allocate extra space for another array; modify the input array in-place with O(1) extra memory. 
 
