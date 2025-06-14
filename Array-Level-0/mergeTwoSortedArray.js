49840cf5116339f52f99c11f8dad4ce07d53ada0
Rahul <rahulmishra4317@gmail.com>
2025-06-15 01:38:57 +0530

    Move zeroes

--- Array-Level-0/mergeTwoSortedArray.js
+++ Array-Level-0/mergeTwoSortedArray.js
@@ -76,12 +76,23 @@ Can you come up with an algorithm that runs in O(m + n) time?
 //     return nums1; 
 // }
 
+
+//Best Approach -> Two pointers without any extra space , doing all the things in-place 
 function mergeTwoSortedArray(nums1 , m , nums2 , n) {
     let p1 = m-1;
     let p2 = n-1;
     for(let i = m+n-1 ; i>=0;i--) {
-        if()
+        if(p2 < 0) break; 
+
+        if(p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[i] = nums1[p1];
+            p1--;
+        } else {
+            nums1[i] = nums2[p2];
+            p2--;
+        }
     }
+    return nums1; 
 }
 
 // --- Test Cases ---
