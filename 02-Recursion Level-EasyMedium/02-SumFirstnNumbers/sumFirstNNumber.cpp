7da1c97baf534aa134e30c84b14e6ddaba68072f
Rahul Mishra <rahulmishra4317@gmail.com>
2025-12-23 01:10:27 +0530

    notes sync

--- 02-Recursion Level-EasyMedium/02-SumFirstnNumbers/sumFirstNNumber.cpp
+++ 02-Recursion Level-EasyMedium/02-SumFirstnNumbers/sumFirstNNumber.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int sumOfNNaturalNumbers(int n) {
     if(n==0) return 0;
-    return (n + sumOfNNaturalNumbers(n-1));
+    return n + sumOfNNaturalNumbers(n-1);
 }
 
 int main() {
