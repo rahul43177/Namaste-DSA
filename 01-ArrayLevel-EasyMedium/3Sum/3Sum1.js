a90b835aa950eb99203f7dedf645f90683643ec3
Rahul Mishra <rahulmishra4317@gmail.com>
2026-02-02 14:09:38 +0530

    ThreeSUm

--- 01-ArrayLevel-EasyMedium/3Sum/3Sum1.js
+++ 01-ArrayLevel-EasyMedium/3Sum/3Sum1.js
@@ -1,38 +1,36 @@
 function threeSum(nums) {
-    //sort 
-    nums.sort((a,b) => a-b); 
-    const n = nums.length; 
-    const res = [] ; 
+    //sort the array first 
+    nums.sort((a,b) => a-b)
 
-    //iterating through i 
+    let n = nums.length; 
+    let res = []; //empty result 
+
     for(let i = 0;i<n;i++) {
-        //checking for the duplicate i after 0 index and if same skipping it 
-        if(i>0 && nums[i] == nums[i+1]) continue; //skip this iteration and go to i 
+        if(i>0 && nums[i] == nums[i-1]) continue; //previous i was also same -- then duplicate and skip this loop using continue 
 
-        let target = -nums[i]; 
         let left = i+1; 
         let right = n-1; 
+        let target = -nums[i]
         while(left < right) {
-            let sum = nums[left] + nums[right]; 
-            if(sum === target) {
+            let s= nums[left] + nums[right]; 
+
+            if(s == target) {
                 res.push([nums[i] , nums[left] , nums[right]]); 
-                left++; 
-                right--; 
+                left+=1 ;
+                right-=1; 
 
 
-                //skipping duplicates for left pointer ; 
-                while(left < right && nums[left] == nums[left-1]) left++; 
-                //skipping duplicates for right pointer ; 
-                while(left < right && nums[right] == nums[right-1]) right--; 
-            } else if(sum < target) {
-                left++; 
+                while(left < right && nums[left] == nums[left-1]) left+=1 ; 
+                while(left < right && nums[right] == nums[right+1]) right-=1; 
+            } else if(s < target) {
+                left+=1 ; 
             } else {
-                right--; 
+                right-=1 ; 
             }
         }
     }
     return res; 
 }
 
-let nums = [-1,0,1,2,-1,-4]
-console.log(threeSum(nums)); 
+let nums =  [-1,0,1,2,-1,-4]
+console.log(threeSum(nums))
